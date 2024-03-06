960284b3aaec887918e8009f3f8ecf7decf473f3
Claas Paaschburg <paaschburgc@gmail.com>
2024-03-06 14:21:22 +0100

    chore: give list items unique properties

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const btnAdd = document.querySelector(".btn-add");
 const input = document.querySelector(".input");
 const list = document.querySelector("ul");
+const tasks = [];
 
 function addInput(event) {
   event.preventDefault();
-  const newTask = document.createElement("li");
-  const taskText = document.createTextNode(input.value);
   if (input.value.trim() !== "") {
-    newTask.appendChild(taskText);
-    list.appendChild(newTask);
+    const newItem = document.createElement("li");
+    const itemText = document.createTextNode(input.value);
+    let newTask = {
+      description: input.value,
+      id: Date.now() + Math.random(),
+    };
+    tasks.push(newTask);
+    newItem.appendChild(itemText);
+    list.appendChild(newItem);
   }
   input.value = "";
 }
