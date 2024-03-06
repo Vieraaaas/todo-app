8b51944ed0c59e17b1c3d4a40f913673dcad9ff5
Claas Paaschburg <paaschburgc@gmail.com>
2024-03-06 10:58:21 +0100

    fix: prevent entries without content

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const list = document.querySelector("ul");
 function addInput() {
   const newTask = document.createElement("li");
   const taskText = document.createTextNode(input.value);
-  newTask.appendChild(taskText);
-  list.appendChild(newTask);
+  console.log(input.value);
+  if (input.value.trim() !== "") {
+    newTask.appendChild(taskText);
+    list.appendChild(newTask);
+  }
+  input.value = "";
 }
 
 btnAdd.addEventListener("click", addInput);
